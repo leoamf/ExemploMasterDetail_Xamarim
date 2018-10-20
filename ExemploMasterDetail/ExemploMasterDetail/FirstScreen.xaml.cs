3e6da79595699e505e48861f26678c2385160dbd
aluno <aluno@LABMAC09.local>
2018-10-20 14:31:41 -0300

    adjusted

--- ExemploMasterDetail/ExemploMasterDetail/FirstScreen.xaml.cs
+++ ExemploMasterDetail/ExemploMasterDetail/FirstScreen.xaml.cs
@@ -10,6 +10,18 @@ namespace ExemploMasterDetail
         public FirstScreen()
         {
             InitializeComponent();
+
+            Label lblNovo = new Label();
+            lblNovo.TextColor = Color.Blue;
+            lblNovo.BindingContext = slider;
+            lblNovo.SetBinding(Label.TextProperty, "Value");
+            stack.Children.Add(lblNovo);
+
+            Label lblNovo2 = new Label();
+            lblNovo2.TextColor = Color.Green;
+            Binding binding = new Binding { Source=slider, Path="Value" };
+            lblNovo2.SetBinding(Label.TextProperty, binding);
+            stack.Children.Add(lblNovo2);
         }
 
     }
